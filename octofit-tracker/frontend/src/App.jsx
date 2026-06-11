ce277c9cc69d25e67ea72eb31f6b8114dc62a8d3
Tobi-acn <kouji.tobisaka@accenture.com>
2026-06-11 15:34:21 +0900

    Add App component

---
+++ octofit-tracker/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🐙 OctoFit Tracker</h1>
+      <p>Welcome to your fitness tracking application!</p>
+    </div>
+  )
+}
+
+export default App
